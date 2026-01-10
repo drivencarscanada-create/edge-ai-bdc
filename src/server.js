adad8df97267a80174dc9464f806f9abd4123f4f
drivencarscanada-create <drivencarscanada@gmail.com>
2026-01-10 14:10:43 -0500

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/health", (req, res) => {
+  res.json({ ok: true });
+});
+
+app.post("/lead", (req, res) => {
+  console.log("NEW LEAD RECEIVED:", req.body);
+  res.json({ ok: true });
+});
+
+app.listen(3007, () => {
+  console.log("Server running on port 3007");
+});
+
